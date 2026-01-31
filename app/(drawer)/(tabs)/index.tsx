af6645fcad4dcd8531efae9f53413af812709b34
test <thach752002@gmail.com>
2026-01-31 16:59:28 +0700

    [Update] Source

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next"
 import {
   ColumnComponent,
   Container,
+  DateTimePicker,
   FlatListComponent,
   FloatButton,
   RowComponent,
@@ -14,11 +15,13 @@ import JarCard from "@/components/jars/jar-card"
 import { useAppBottomSheet } from "@/contexts/bottom-sheet-provider"
 import useStore from "@/store"
 import { Jar } from "@/types"
+import { useState } from "react"
 
 export default function JarsScreen() {
   const { t } = useTranslation()
   const { userData } = useStore()
   const { openSheet, closeSheet } = useAppBottomSheet()
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
 
   const {
     jars,
@@ -43,6 +46,11 @@ export default function JarsScreen() {
     <Container>
       <ColumnComponent gap={20} style={{ paddingTop: 5 }}>
         <ColumnComponent gap={10}>
+          <DateTimePicker
+            mode="date"
+            dateTime={selectedDate}
+            setDateTime={setSelectedDate}
+          />
           <TextComponent
             text={t("good morning") + ", " + (userData?.full_name || '')}
             type="label"
